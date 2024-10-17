7ab379ace33b91bb4e67a239cbacdd818306c024
hkarani <hezronkaranii@gmail.com>
2024-10-17 18:24:13 +0300

    Fix healthchecks

--- scripts/procedures/healthChecks.ts
+++
@@ -1,7 +0,0 @@
-import { types as T, healthUtil } from "../deps.ts";
-
-export const health: T.ExpectedExports.health = {
-  async "interface"(effects, duration) {
-    return healthUtil.checkWebUrl("http://phoenixd-lightning-wallet-ui.embassy:3000")(effects, duration).catch(healthUtil.catchError(effects))
-  },
-};
