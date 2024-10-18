3c8d35ae9a7a8035269c3b85d9e76e3aa885a006
hkarani <hezronkaranii@gmail.com>
2024-10-18 11:04:58 +0300

    Check UI health inside the container

--- check-phoenixd.sh
+++ check-phoenixd.sh
@@ -1,12 +1,17 @@
+#!/bin/bash
 
 DURATION=$(</dev/stdin)
-if (($DURATION <= 30000 )); then 
+if (($DURATION <= 30000)); then
+    echo "Service is still initializing"
     exit 60
 else
-    curl --silent --fail phoenixd-lightning-wallet-ui.embassy &>/dev/null
-    exit_code=$?
-    if test "$exit_code" != 0; then
-        echo "The RTL UI is unreachable" >&2
+    # Check if the Phoenixd service and Node.js app are running inside the container
+    if curl --silent --fail http://127.0.0.1:3000 &>/dev/null; then
+        echo "Service is healthy"
+        exit 0
+    else
+        echo "Service is unreachable inside the container" >&2
         exit 1
     fi
 fi
+
